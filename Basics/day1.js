8eaa35336a2ab5b840fc4ed5d18c6ee6d6d85a61
jatin504 <jt872678@gmail.com>
2024-06-01 00:08:42 +0530

    initial commit

---
+++ Basics/day1.js
@@ -0,0 +1,16 @@
+// let, var, const
+
+const accountId = 14432
+let accountEmail = "code@gmail.com"
+var accountPass = "12345"
+accountCity = "Delhi"
+
+// accountId = 131 // Assignment to constant variable.
+accountEmail="cgcg@gmail.com"
+accountPass = "212121"
+accountCity="pune"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPass);
+console.table([accountId,accountEmail,accountPass,accountCity])
---
+++ Basics/day2.js
@@ -0,0 +1,22 @@
+//"use strict"; //treat all js code as newer vesrion 
+
+//alert(3+3) // we are using node js, not browswer so that's why it is not run in our vs code or any compiler
+
+//console.log(3+3); console.log("hello") //not a good practise for good coder
+
+
+// Data Types
+
+// primitive
+//Number => 2
+// bigint
+//String => "djskd"
+//Boolean => true/false
+// null =>standalone value
+// undefined => not assign at time
+// symbol => unique
+
+// non-primitive data type
+//object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined

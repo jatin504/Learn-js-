55664886d71a6f9ca1b22a64636190f7c1210d30
jatin504 <jt872678@gmail.com>
2024-06-01 23:56:11 +0530

    datatypes

--- Basics/day3.js
+++ Basics/day3.js
@@ -1,12 +1,12 @@
 // datatype conversion
 
 // let num = 33 //num
-let num = null //string
+//let num = null //string
 //let num = true //boolean
 
-let ValueInNumber = Number(num)
-console.log(typeof(ValueInNumber))
-console.log(ValueInNumber)
+// let ValueInNumber = Number(num)
+// console.log(typeof(ValueInNumber))
+// console.log(ValueInNumber)
 
 
 // "33" => 33
@@ -18,9 +18,9 @@ console.log(ValueInNumber)
 
 // Converting a datatypes into boolean
 
-let isLogged = undefined
-let valueInBoolean = Boolean(isLogged)
-console.log(valueInBoolean)
+// let isLogged = undefined
+// let valueInBoolean = Boolean(isLogged)
+// console.log(valueInBoolean)
 
 // 1 => true; 
 // "abc" => true
@@ -30,10 +30,64 @@ console.log(valueInBoolean)
 
 // Converting a datatypes into string
 
-let str = 1.2;
-let valueInString = String(str)
-console.log(valueInString)
-console.log(typeof(valueInString))
+// let str = 1.2;
+// let valueInString = String(str)
+// console.log(valueInString)
+// console.log(typeof(valueInString))
 
 // 33 => string
 // all value in string
+
+// -----------------------operation-----------------------
+
+// let value =3
+// let negvalue= -value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+// let str1 = 'hello'
+// let str2 = 'code'
+// let str3 = str1+str2
+// console.log(str3)
+
+// dont'do this
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log((3+4)*5%3)
+// console.log(+true)
+// console.log(+"")
+
+// let num1, num2, num3 
+
+// num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100;
+// gameCounter++; // prefix before increment
+//++gamecounter //postfix after increment
+// console.log(gameCounter)
+
+// comparison operator
+// console.log(2>3)
+// console.log(2<3)
+// console.log(2>=3)
+// console.log(2<=3)
+// console.log(2==3)
+// console.log("2">="3")
+
+// avoid this type of conversion
+//console.log(null>0)
+//console.log(null==0) // null as 0
+//console.log(null>=0) //comparisons convert null to a number, treating it as 0.,,,,,That's why(3) null>0 is true and (1) null>0 is false
+//console.log(undefined==0)
+
+//strict check
+//console.log("2"===2)
+
---
+++ day4.js
@@ -0,0 +1,19 @@
+// Data types of javascript
+
+// -> js is a dynamic type
+
+// primitive datatype
+
+// 7 types :- 
+
+//String,Number,Boolean,null,undefined,BigInt,symbol
+
+// non primitive
+
+// Array, object, function
+
+
+
+
+//read this
+//https://262.ecma-international.org/5.1/#sec-11.4.3 

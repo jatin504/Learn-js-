9e657dd174e2a83558ab43b9e6568dffe84c7956
jatin504 <jt872678@gmail.com>
2024-06-06 00:08:07 +0530

    day 8 object

---
+++ Basics/day8.js
@@ -0,0 +1,31 @@
+// objects
+
+// single ton => if we declare as literals so they create a single ton 
+
+const jsuser ={
+    name:"romio",
+    "lastname":"zone",
+    age:20,
+    isLogged:true,
+    cars:["BMW","AUDI"]
+}
+console.log(jsuser.name)
+console.log(jsuser["name"])
+console.log(jsuser["lastname"])
+
+
+
+// object destructure
+const course ={
+    coursename:"javascript",
+    price:2000,
+    courseinstructor:"Documentation"
+}
+// const {courseinstructor} = course
+// const {courseinstructor: instructor} = course
+// console.log(course);
+// console.log(instructor);
+
+
+
+
